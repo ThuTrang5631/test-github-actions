99554343456705f3ab5c1f54d961b234cae13be4
trang.le <thutrang5631@gmail.com>
2026-07-20 22:29:38 +0700

    fix: remove param

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,7 +2,6 @@ export default function BlogPostPage({ params }: { params: { slug: string } }) {
   return (
     <main>
       <h1>Blog post page (test build)</h1>
-      <p>{params?.slug}</p>
     </main>
   );
 }
